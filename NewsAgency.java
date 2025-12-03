9837db92640caa3a57ef999b18621dd793cde812
Bai Sakina Abad <kimireneabad07@gmail.com>
2025-12-04 02:28:16 +0800

    Initial commit

--- NewsAgency.java
+++ NewsAgency.java
@@ -1,2 +1,4 @@
 public class NewsAgency {
+    //Represents the central hub for publishing news. It maintains a list of subscribers and notifies them whenever new news is available.
+
 }
--- Subscriber.java
+++ Subscriber.java
@@ -1,2 +1,4 @@
 public class Subscriber {
+    //Represents users or entities subscribed to the news service. Subscribers receive immediate notifications about any breaking news published by the agency.
+
 }
